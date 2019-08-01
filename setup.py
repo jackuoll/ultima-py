9d23aa33af0c78361d47d65881c2c6b92cdc1725
Jack Ward <jack.ward@grabone.co.nz>
2019-08-01 18:47:37 +1200

    fix setup

--- setup.py
+++ setup.py
@@ -5,8 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="ultimapy",
-    version="0.0.6a",
-    version="0.0.5",
+    version="0.0.7",
     author="Jack Ward",
     author_email="jackward84@gmail.com",
     description="UltimaPy - Extract information and images from the UO client files",
